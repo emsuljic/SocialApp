a332285400c280a1e37892f26515e899b69c5aef
emsuljic <esuljic5@gmail.com>
2021-03-11 19:16:14 +0100

    End of section 10

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,21 +1,49 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
 @Injectable({
   providedIn: 'root'
 })
-export class MembersService {
 
+export class MembersService {
   baseUrl = environment.apiUrl;
+  members: Member[] = [];
   
   constructor(private http: HttpClient) { }
 
   getMembers(){
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    //check if there is members, and return them from service as obesrvable of(this.member)
+    if(this.members.length > 0) return of(this.members);
+    //set members
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+    )
   }
+
   getMember(username: string){
+    //get member from service Member
+    //finding member with the sam eusername that we parsing throw parameter
+    const member = this.members.find(x => x.username === username);
+    //if member in find() is not undefined 
+    if(member !== undefined) return of(member);
+    //if we don't have a member we call him from API call
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+
+  updateMember(member: Member){
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        //get member from the service, find index of member
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    )
   }
 }
