b5c4977b9cc22e640914f1c9fa1e677217eac501
emsuljic <esuljic5@gmail.com>
2021-03-24 21:18:52 +0100

    Section 19 - pre switch to postgres

--- API/Controllers/FallbackController.cs
+++ API/Controllers/FallbackController.cs
@@ -6,11 +6,11 @@ namespace API.Controllers
     //if API dosen't know what to do with route, then it falls back to this controller and return this particular file
     //index.html is responsible for loading our Angular app
     //Angular app definetley knows what to do with the routes, for the Angualr application
-        public class FallbackController: Controller
+    public class FallbackController : Controller
     {
         public ActionResult Index()
         {
-            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), 
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
                 "wwwroot", "index.html"), "text/HTML");
         }
     }
